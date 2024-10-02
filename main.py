8986af4a759938afb1e92cc4a7841c970c59543a
ClapedByPolski <wioleta.bratkowska@icloud.com>
2024-10-02 19:19:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import Time
+import random
+fakty = []
+for i in range(3):
+    fakt = input("dodaj info o siebie")
+    fakty.append(fakty)
+    print(fakty)
+
+print("losowanie")
+time.sleep(3)
+print("losowany fakt to", random.choice(fakty))
